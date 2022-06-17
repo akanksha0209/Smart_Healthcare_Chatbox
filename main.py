5aa4c6ec90768cfcdb36dc6448afe89002ddb83a
Akanksha <admin@gmail.com>
2022-06-17 19:08:15 +0530

    app.py doubtful

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, redirect
+from __future__ import division, print_function
+
+from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Length
 from sqlalchemy import Column, Integer, String
+import sys
+import os
+import glob
+import re
+import numpy as np
+from PIL import Image as pil_image
+
+from tensorflow.keras.applications.imagenet_utile import preprocess_input, decode_predictions
+from tensorflow.keras.model import Model, load_model
+from keras.preprocessing import image
+from werkzeug.utils import secure_filename
+
+from gevent.pywsgi import WGSIServer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
@@ -14,6 +29,27 @@ db = SQLAlchemy(app)
 # secret key
 app.secret_key = "akanksha"
 Bootstrap(app)
+
+Model = load_model('model12345.h5')
+
+lesion_classes_dict = {
+    0: 'Melanocytic nevi',
+    1: 'Melanoma-Cancerous',
+    2: 'Benign keratosis-like lesions',
+    3: 'Basal cell carcinoma-Cancerous',
+    4: 'Actinic keratoses',
+    5: 'Vascular lesions',
+    6: 'Dermatofibroma'
+
+}
+
+
+def model_predict(image_path, Model):
+    img = image.load_img(image_path, target_size=(224, 224, 3))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    preds = Model.predict(x)
+    return preds
 
 
 class SignUpForm(FlaskForm):
@@ -84,9 +120,22 @@ def login():
         else:
             return render_template('login.html', form=login_form)
 
-
-
     return render_template('login.html', form=login_form)
+
+
+@app.route('/predict', methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        preds = model_predict(file_path, Model)
+        pred_class = preds.argmax(axis=-1)
+        pr = lesion_classes_dict[pred_class[0]]
+        result = str(pr)
+        return result
+    return None
 
 
 if __name__ == '__main__':
